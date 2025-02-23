419c3a5150121416acbc700b9dd25ef8e2a9adc1
DevExplorer04 <govardhanpatil2600@gmail.com>
2025-02-23 21:34:21 +0530

    New Create Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Technologies from "./Components/Technologies";
 
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antiliased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="flixed top-0 -z-10 h-full w-full">
-        <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       </div>
 
-      <NavBar />
+      <div className="container mx-auto px-8">
+        <NavBar />
+        <Hero />
+        <About />
+        <Technologies/>
+      </div>
     </div>
   );
 };
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,22 +1,25 @@
-import logo from "../assets/kevinRushLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+
 const NavBar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-centre">
-        <img src={logo} alt="logo" />
+      <div className="flex flex-shrink-0 items-center">
+        <h1 className="mt-2 bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-4xl tracking-tight text-transparent animate-gradient">
+          CapGemini
+        </h1>
       </div>
-      <div className="m-8  flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaSquareXTwitter />
-        <FaInstagram />
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin className="text-blue-600" />
+        <FaGithub className="text-black-400" />
+        <FaSquareXTwitter className="text-black-600" />
+        <FaInstagram className="text-pink-600"/>
       </div>
     </nav>
   );
-}; 
+};
 
 export default NavBar;
+  
