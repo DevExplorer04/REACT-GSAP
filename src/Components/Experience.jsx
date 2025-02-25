a4c3210d93710af0d89903be067a7ae27e21460b
GitChalo <GitChalo>
2025-02-25 14:36:43 +0530

    Test

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -9,6 +9,23 @@ const Experience = () => {
             <div className="w-full lg:w-1/4">
               <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
             </div>
+            <div className="w-full max-w-xl lg:w-3/4">
+              <h6 className="mb-2 font-semibold">
+                {experience.role}-
+                <span className="text-sm text-purple-100">
+                  {experience.company}
+                </span>
+              </h6>
+              <p className="mb-4 text-neutral-400 ">{experience.description}</p>
+              {experience.technologies.map((tec, index) => (
+                <span
+                  key={index}
+                  className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-pink-800"
+                >
+                  {tec}
+                </span>
+              ))}
+            </div>
           </div>
         ))}
       </div>
